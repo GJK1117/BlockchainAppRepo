9a553f2b992e62682d9075bbcd5d371edcd6b229
김준곤 <118426175+GJK1117@users.noreply.github.com>
2024-04-04 10:31:18 +0900

    코드 추가

---
+++ Chapter4/Ex4_1.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Ex4_1{
+
+    function fun1(uint a) public pure returns(uint){
+        if(a>=10){
+            a = 9;
+        }
+        return a;
+    }
+}
---
+++ Chapter4/Ex4_7.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Ex4_7{
+    
+    function fun1() public pure returns(uint){
+        uint result = 0;
+        uint a = 3;
+        while(a>0){
+            result = result + a;
+            --a;
+        }
+        return result;
+    }
+}
